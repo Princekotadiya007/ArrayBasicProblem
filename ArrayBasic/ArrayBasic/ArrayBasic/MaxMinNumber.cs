608e9ab559fda1c15bc88e598d78deba34458cec
Princekotadiya007 <princuuu77@gmail.com>
2023-02-14 11:16:24 +0530

    [prince] max min number

---
+++ ArrayBasic/ArrayBasic/ArrayBasic/MaxMinNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBasic
+{
+    public class MaxMinNumber
+    {
+        public void MinMax()
+        {
+
+            int[] numbers = new int[] { 10, 20, 30, 40, 50 };
+
+            int max = numbers[0];
+            int min = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            Console.WriteLine("The maximum element in the array is: " + max);
+            Console.WriteLine("The minimum element in the array is: " + min);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ArrayBasic/ArrayBasic/ArrayBasic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBasic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 ArrayLength");
+            Console.WriteLine("choose the option");
+            int opiton = Convert.ToInt32(Console.ReadLine());
+
+            switch (opiton)
+            {
+                case 1:
+                    MaxMinNumber minMaxNUmber = new MaxMinNumber();
+                    minMaxNUmber.MinMax();
+                    break;
+            }
+        }
+    }
+}
